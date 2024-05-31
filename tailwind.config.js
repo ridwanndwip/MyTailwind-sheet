0ee27e4c2865d8d404db58e565b5838489eaeefb
ridwanndwip <ridwanndwip@gmail.com>
2024-05-31 10:04:24 +0700

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
       extend: {
-        backgroundImage: {
+      colors:{
+        line:'#232C3E',
+        text:'#16FF00',
+      },
+      backgroundImage: {
             'hero-pattern':
-              "linear-gradient(15deg, #d33f34 50%, #a61322 50.1%),",
+            "linear-gradient(15deg, #d33f34 50%, #a61322 50.1%),",
           },
       },
     },
